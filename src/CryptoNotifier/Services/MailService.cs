874bf9e510ca67ee5ae9038c521fe313c78fe726
Luis Ortiz <luisfmazzu@hotmail.com>
2021-05-16 18:08:00 -0300

    Fixes mail sending after one loop has passed

--- src/CryptoNotifier/Services/MailService.cs
+++ src/CryptoNotifier/Services/MailService.cs
@@ -23,29 +23,63 @@ namespace CryptoNotifier.Services
 
         public void Send(string subject, string message)
         {
-            //Send email
-            using (MailMessage mailMessage = new MailMessage("cryptonotifiersbros@gmail.com", "henrique.mazzu@gmail.com")
-            {
-                Subject = subject,
-                Body = message,
-                IsBodyHtml = true,
-                BodyEncoding = System.Text.Encoding.UTF8,
+            int tryAgain = 10;
+            bool failed = false;
 
-            })
+            do
             {
-                smtpClient.Send(mailMessage);
-            }
-            using (MailMessage mailMessage = new MailMessage("cryptonotifiersbros@gmail.com", "luisfmazzu@gmail.com")
-            {
-                Subject = subject,
-                Body = message,
-                IsBodyHtml = true,
-                BodyEncoding = System.Text.Encoding.UTF8,
+                try
+                {
+                    failed = false;
 
-            })
+                    var smtp = new SmtpClient("smtp.gmail.com")
+                    {
+                        Port = 587,
+                        Credentials = new NetworkCredential("cryptonotifierbros@gmail.com", "cryptonotifier123"),
+                        EnableSsl = true,
+                    };
+                    var mail = new MailMessage("cryptonotifiersbros@gmail.com", "henrique.mazzu@gmail.com");
+                    mail.Subject = subject;
+                    mail.Body = message;
+                    mail.IsBodyHtml = true;
+                    smtp.Send(mail);
+                }
+                catch (Exception ex)
+                {
+                    failed = true;
+                    tryAgain--;
+                    var exception = ex.Message.ToString();
+                }
+            } while (failed && tryAgain != 0);
+
+            tryAgain = 10;
+            failed = false;
+
+            do
             {
-                smtpClient.Send(mailMessage);
-            }
+                try
+                {
+                    failed = false;
+
+                    var smtp = new SmtpClient("smtp.gmail.com")
+                    {
+                        Port = 587,
+                        Credentials = new NetworkCredential("cryptonotifierbros@gmail.com", "cryptonotifier123"),
+                        EnableSsl = true,
+                    };
+                    var mail = new MailMessage("cryptonotifiersbros@gmail.com", "luisfmazzu@gmail.com");
+                    mail.Subject = subject;
+                    mail.Body = message;
+                    mail.IsBodyHtml = true;
+                    smtp.Send(mail);
+                }
+                catch (Exception ex)
+                {
+                    failed = true;
+                    tryAgain--;
+                    var exception = ex.Message.ToString();
+                }
+            } while (failed && tryAgain != 0);
         }
     }
 }
